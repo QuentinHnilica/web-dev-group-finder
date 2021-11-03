c5127992fc7d8b5581052a4345cae2fbc186c526
unknown <Quentinhnilica@gmail.com>
2021-11-03 15:40:46 -0700

    User Seeds

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,16 @@
-const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
+const Sequelize = require('sequelize');
 
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: '127.0.0.1',
-      dialect: 'mysql'
-    }
-  );
-
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
 
 module.exports = sequelize;
--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,4 @@
 DROP DATABASE IF EXISTS group_finder_db;
 CREATE DATABASE group_finder_db;
+
+use group_finder_db;
