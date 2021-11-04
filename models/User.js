cb4d134f43568fac1baf35c79f27f98a5f66d1f7
letqin <86627655+letqin@users.noreply.github.com>
2021-11-03 21:39:37 -0400

    bcrypt conflicts

--- models/User.js
+++ models/User.js
@@ -1,7 +1,12 @@
-const { DataTypes, Model} = require('sequelize');
+const { DataTypes, Model } = require('sequelize');
+const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class User extends Model {}
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    };
+};
 
 User.init(
     {
@@ -24,15 +29,15 @@ User.init(
             validate: {
                 isEmail: {
                     msg: "Must be a valid email address",
-                }
-            }
+                },
+            },
         },
         password: {
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
                 len: [8],
-            }
+            },
         },
         isAdmin: {
             type: DataTypes.BOOLEAN,
@@ -44,6 +49,12 @@ User.init(
         }
     },
     {
+    hooks: {
+        beforeCreate:async(newUserData) => {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+        },
         sequelize,
         timestamps: false,
         freezeTableNames: true,
