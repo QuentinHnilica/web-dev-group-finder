52fb2b1c3fa31a54e67e0e4f7d86f404c38d04dd
unknown <Quentinhnilica@gmail.com>
2021-11-06 08:20:19 -0700

    You can now log in horray

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const routes = require('./controllers');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
 
 const app = express();
 
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 
 const PORT = process.env.PORT || 3001;
 
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'))
 
 const hbs = exphbs.create();
 
