87efcd8cc10b17cec01cc2eb24f3290e623cd69d
unknown <Quentinhnilica@gmail.com>
2021-11-08 16:46:30 -0800

    We can display data from the data base now. Horry. Also Quinn helped alot in this bit

--- controllers/api/projectRoutes.js
+++ controllers/api/projectRoutes.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
 const { Project } = require('../../models');
-
-router.get('/projects', async(req, res) =>{
-    await Project.findAll()
+//match /api/projects/
+router.get('/', async(req, res) =>{
+    try{
+    const projects = await Project.findAll()
+    res.status(200).json(projects)
+    }catch(err) {
+        res.status(400).json(err);
+    }
 })
 
 module.exports = router;
--- public/assets/js/findGroup.js
+++ public/assets/js/findGroup.js
@@ -1,43 +1,46 @@
+const findGroupHandler = async (event) =>{
+    let groupInfo
+    const response = await fetch("/api/projects/", {
+        method: "GET"
+    })
+    if (response.ok) {
+        response.json().then(function(data){
+            //data is the json file
+            for (let i = 0; i < data.length; i++){
+                if (i == 6 ){
+                    break;
+                }
+                //description is an object value
+                testVar = data[i].description
+                const groupPost = 
+                `<div class = "d-flex flex-column align-items-center">
+                <section class = "border m-4" style="width: 90%;">
+                <div class = "d-flex align-items-start flex-column m-3">
+                    <h3 class="boxTextColor">Group name</h3>
+                    <p class="boxTextColor">${testVar}</p>
+                </div>
+                <!-- bottom of sub-box -->
+                <div class = "d-flex justify-content-between m-3">
+                    <div>
+                        <h6 class="boxTextColor">Needs:</h6>
+                        <!-- iconHolder -->
+                        <a></a>
+                    </div>
+                    <button class = "btn bodyButtons">View Group</button>
+                </div>
+                </section>
+                </div>`
+                $('#postGroup').append(groupPost)
+            }
+        })
+    }
+    else{
+        alert(response.statusText);
+    }
+
 let testVar = "Hello World"
 
 const insertPost = document.getElementById('postGroup')
 
-
-
-function loadPosts(){
-    
-
-    for (let i = 0; i < 5; i++){
-        if (i == 1){
-            testVar = "Hello World"
-        }
-        else if (i == 2){
-            testVar = "I can't believe this works"
-        }
-        else{
-            testVar = "Yessir!"
-        }
-
-
-        const groupPost = `<div class = "d-flex flex-column align-items-center">
-        <section class = "border m-4" style="width: 90%;">
-        <div class = "d-flex align-items-start flex-column m-3">
-            <h3 class="boxTextColor">Group name</h3>
-            <p class="boxTextColor">${testVar}</p>
-        </div>
-        <!-- bottom of sub-box -->
-        <div class = "d-flex justify-content-between m-3">
-            <div>
-                <h6 class="boxTextColor">Needs:</h6>
-                <!-- iconHolder -->
-                <a></a>
-            </div>
-            <button class = "btn bodyButtons">View Group</button>
-        </div>
-        </section>
-        </div>`
-        $('#postGroup').append(groupPost)
-    }
 }
-
-loadPosts()
+findGroupHandler()
